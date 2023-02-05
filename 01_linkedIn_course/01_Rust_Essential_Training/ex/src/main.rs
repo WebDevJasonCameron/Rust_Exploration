84a488a700b9d399ac6f5c7188dcabe7ea4f747d
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-05 16:44:43 -0600

    start ex 8

--- 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
+++ 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
@@ -57,3 +57,15 @@ fn main() {
     println!("value is {value:08b}");
 }
 */
+
+// Ex 8
+fn main() {
+    let a = true;
+    let b = false;
+
+    println!("a is {a} and b is {b}");
+    println!("NOT a is {}", !a);
+    println!("a AND b is {}", a & b);
+    println!("a OR b is {}", a | b);
+    println!("a XOR b is {}", a ^ b);
+}
