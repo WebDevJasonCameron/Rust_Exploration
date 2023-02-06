be445d429b514c37b2c4cc1fa0cc3f907b8aa06a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-05 19:06:38 -0600

    start 08

--- 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
+++ 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
@@ -58,7 +58,8 @@ fn main() {
 }
 */
 
-// Ex 8
+// Ex 8 part a and b
+/*
 fn main() {
     let a = true;
     let b = false;
@@ -68,4 +69,27 @@ fn main() {
     println!("a AND b is {}", a & b);
     println!("a OR b is {}", a | b);
     println!("a XOR b is {}", a ^ b);
+
+    let c = (a ^ b) && panic!("broke");
+    println!("c is {c}");
+}
+ */
+/*
+fn main() {
+    let a = 1;
+    let b = 2;
+    println!("a is {} and b is {}", a, b);
+    println!("a EQUAL TO  b is {}", a == b);
+    println!("a NOT EQUAL TO b is {}", a != b);
+    println!("a GREATER THAN b is {}", a > b);
+    println!("a GREATER THAN OR EQUAL TO b is {}", a >= b);
+    println!("a LESS THAN b is {}", a < b);
+    println!("a LESS THAN OR EQUAL TO b is {}", a <= b);
+}
+ */
+
+// Ex 08
+fn main() {
+    let letter = 'a';
+    let number = '1';
 }
