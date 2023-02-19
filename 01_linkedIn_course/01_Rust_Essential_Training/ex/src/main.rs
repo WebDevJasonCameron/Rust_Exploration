d5362bdb9264b2a787df69d6c756522a8722df96
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-18 19:42:03 -0600

    updating some of the old stuff for tomorrow

--- 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
+++ 01_linkedIn_course/01_Rust_Essential_Training/ex/src/main.rs
@@ -519,3 +519,16 @@ fn main() {
 */
 
 // Crate
+use rand::prelude::*;
+
+fn main() {
+    let number = random::<f64>();
+    println!("number is {number}");
+
+    let number = thread_rng().gen_range(1..11);
+    println!("number is {number}");
+}
+
+fn random2() -> f64 {
+    1.0
+}
